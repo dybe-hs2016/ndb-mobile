1715a7e19e2501c3d1847f12e094e2114ce653d2
Christine Lehmann <clehmann@a2er.ch>
2016-11-27 18:01:14 +0100

    SQL-Befehle eingefügt - funktioniert noch nicht

--- ndb-mobile-bs/bdy.03-01.entry.tbl.php
+++ ndb-mobile-bs/bdy.03-01.entry.tbl.php
@@ -1,5 +1,37 @@
+<?php
+	require_once('verbindung.php');
+	
+	//Suchabfrage für die Felder der Tabelle Noten
+	$sql_n = "SELECT * FROM `tbl_noten` WHERE `id`=`".$_GET['id']."` ";	
+	$query_n = mysqli_query($verb, $sql_n) or die("Fehler:".mysqli_error($verb));
+	$detail_n = mysqli_fetch_assoc($query_n);
+	
+	//Suchabfragen für die Felder der Tabelle Komponist, Instrument und Verlag
+	//Diese Felder werden nur angezeigt, wenn sie auch einen Wert enthalten
+	if($detail_n["id_komponist"]) {
+		$sql_k = "SELECT * FROM ".$tbl_komponist." WHERE id_komponist=".$detail_n["id_komponist"]."; ";
+		$query_k = mysqli_query($verb, $sql_k) or die("Fehler:".mysqli_error($verb));
+		$detail_k = mysqli_fetch_assoc($query_k);
+	}
+	//if($detail_n["id_instrument"]) {
+	//	$sql_i = "SELECT * FROM ".$tbl_instrument." WHERE id_instrument=".$detail_n["id_instrument"]."; ";		
+	//	$query_i = mysqli_query($verb, $sql_i) or die("Fehler:".mysqli_error($verb));
+	//	$detail_i = mysqli_fetch_assoc($query_i);
+	//}
+	//if($detail_n["id_verlag"]) {
+	//	$sql_v = "SELECT * FROM ".$tbl_verlag." WHERE id_verlag=".$detail_n["id_verlag"]."; ";		
+	//	$query_v = mysqli_query($verb, $sql_v) or die("Fehler:".mysqli_error($verb));
+	//	$detail_v = mysqli_fetch_assoc($query_v);
+	//}
+        
+	// Verbindungsprobleme anzeigen
+	echo mysqli_error($verb);
+?>
+
+
+
 <!-- ENTRY METADATA TABLE -->
-<h1>Die Seeräuber Jenny</h1>
+<h1><?php echo $title ;?></h1>$nErfassen
 
 <div class="table">
 	<table class="table">
