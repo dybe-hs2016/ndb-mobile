ffa56f5c0f6d4f23253e399971fc2e7aae972425
tbu <tobias.buchinger@iw.htwchur.ch>
2016-12-09 08:27:43 +0100

    insert correct variables, took out table variables.

--- ndb-mobile-bs/verbindung.php
+++ ndb-mobile-bs/verbindung.php
@@ -1,29 +1,11 @@
 <?php
-/*
-// Wichtigste Verbindungsparameter als Variablen für HTW-Webserver --> Tobias sollte das einfüllen...
-$host = "localhost";
-$db = "321006_1_1";
-$user = "321006_1_1";
-$pwd = "RNoJqKY9cave";
-$tbl = "tbl_pokemon2";
-$verb = mysqli_connect($host, $user, $pwd, $db);
-*/
-
 // Wichtigste Verbindungsparameter als Variablen für lokalen Gebrauch
 $host = "localhost";
-$db = "ndb";
-$user = "root";
-$pwd = "root";
-$noten_instrument = "noten_instrument";
-$tbl_epoch = "tbl_epoch";
-$tbl_instrument = "tbl_instrument";
-$tbl_composer = "tbl_composer";
-$tbl_levels = "tbl_levels";
-$tbl_musicstyle = "tbl_musicstyle";
-$tbl_noten = "tbl_noten";
-$tbl_occasion = "tbl_occasion";
-$tbl_publisher = "tbl_publisher";
-$tbl_tonality = "tbl_tonality";
+$db = "313563_4_1";
+$user = "313563_4_1";
+$pwd = "zBYnZge5RLFt";
+$tbl = "tbl_noten";
+
 $verb = mysqli_connect($host, $user, $pwd, $db);
 
 // Fehler ausgeben
