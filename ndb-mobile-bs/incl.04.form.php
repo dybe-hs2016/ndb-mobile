d8c3f5c167f8ecd55ecb4f466b97c21ad9fee337
Christine Lehmann <clehmann@a2er.ch>
2016-12-22 16:46:49 +0100

    Test ob einzelne Felder abgefragt werden können

--- ndb-mobile-bs/incl.04.form.php
+++ ndb-mobile-bs/incl.04.form.php
@@ -14,7 +14,8 @@
 	$query_freitext = mysqli_query($verb, $sql_freitext) or die("Fehler:".mysqli_error($verb));
 	$suche_freitext = mysqli_fetch_assoc($query_freitext);
 		
-	/* array(15) { ["id"]=> string(1) "1" ["title"]=> string(8) "Sinfonie" ["signature"]=> string(4) "HA 1" 
+	/* var_dump($sql_freitext); ergibt folgendes
+	array(15) { ["id"]=> string(1) "1" ["title"]=> string(8) "Sinfonie" ["signature"]=> string(4) "HA 1" 
 	/["linktomusic"]=> NULL ["linktosheet"]=> NULL ["comment"]=> string(10) "Super Sach" ["id_composer"]=> string(1) "1" 
 	["id_publisher"]=> string(1) "2" ["id_epoch"]=> string(1) "3" ["id_musicstyle"]=> string(1) "4" ["id_tonality"]=> string(2) "35" 
 	["id_levels"]=> string(1) "1" ["id_occasion"]=> string(1) "4" ["name"]=> string(5) "Pärt" ["firstname"]=> string(4) "Arvo" }
@@ -29,8 +30,20 @@
 
 
 <!-- Expertensuche -->
-<!--
-    // $sql_freitext = "SELECT * FROM ".$tbl_noten." "; 
+<?php
+	// Feld Titel
+	if(isset($_POST['suche_title'])) {
+		$sql_freitext = "SELECT `title` FROM ".$tbl_noten." WHERE `title` LIKE '%".$_POST['suche_title']."%' ";
+		} elseif(isset($_POST['suche_composer'])) {
+		$sql_freitext = "SELECT `name` FROM ".$tbl_composer." WHERE `name` LIKE '%".$_POST["suche_composer"]."%' OR `firstname` LIKE '%".$_POST['suche_composer']."%' ";	
+	}
+	$query_freitext = mysqli_query($verb, $sql_freitext) or die("Fehler:".mysqli_error($verb));
+	$suche_freitext = mysqli_fetch_assoc($query_freitext);
+	echo mysqli_error($verb);
+?>	
+
+<!--	
+	    $sql_titel = "SELECT 'title' FROM ".$tbl_noten." "; 
 	// $sql_freitext .= "LEFT JOIN ".$tbl_komponist." ON ".$tbl_noten.".id_komponist=".$tbl_komponist.".id_komponist " ;	
 	// $sql_freitext .= "LEFT JOIN ".$tbl_verlag." ON ".$tbl_noten.".id_verlag=".$tbl_verlag.".id_verlag " ;
 	// $sql_freitext .= "LEFT JOIN ".$tbl_instrument." ON ".$tbl_noten.".id_instrument=".$tbl_instrument.".id_instrument " ;
