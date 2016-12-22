049337f493f15393a90dd57283919aa879934f5b
Christine Lehmann <clehmann@a2er.ch>
2016-12-22 14:14:31 +0100

    Random mit foreach-Schleife zuoberst

--- ndb-mobile-bs/bdy.03.zfTreffer.php
+++ ndb-mobile-bs/bdy.03.zfTreffer.php
@@ -1,4 +1,34 @@
-<!-- ENTRY METADATA TABLE -->
+<!-- Zufallstreffer -->
+
+
+<?php include("incl.04.form.php"); ?>
+
+<?php 
+	echo '<h1>'.$random['title'].'</h1>';
+?>
+
+	<div class="table">
+		<table class="table">
+			<!-- <tr> ID BEZEICHNUNG METADATENFELD
+				<th class="key">BEZEICHUNG</th>
+				<th class="val">INHALT</th>
+			</tr> -->
+
+			<?php 
+				foreach ($random as $key => $val) {			
+					echo '<tr> 
+						<th class="key">'.$key.'</th>
+						<th class="val">'.$val.'</th> 
+					</tr>';
+				}
+			?>
+		</table>
+	</div>	
+
+	
+
+<button onclick='history.back()'>Zur&uuml;ck zur Suche</button>
+<br><br>
 
 <?php
 	$getData = file_get_contents("db-entry-dummy.json");
