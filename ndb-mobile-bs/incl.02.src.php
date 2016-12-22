d7d1dbabf280a85562513bbb7071b9d9a2505925
Christine Lehmann <clehmann@a2er.ch>
2016-12-22 16:47:18 +0100

    Such"sql" eingebaut, einfache Suche klappt

--- ndb-mobile-bs/incl.02.src.php
+++ ndb-mobile-bs/incl.02.src.php
@@ -1,18 +1,23 @@
 <!-- SEARCH FORM vs INPUT -->
-<div class="input-group">									
-	<!-- <form action="get"> -->
-	<!-- <label class="sr-only" for="src"> search </label> -->
 
-		<input 	type="search" class="form-control" id="src" placeholder="Suche..." formaction="results.html">		
+<!-- include SQL -->
+<?php include("incl.04.form.php"); ?>
 
-		<span class="input-group-btn">
-			<button class="btn btn-default btn-src" type="button">
-				<span class="glyphicon glyphicon-search"></span>
-			</button>
-		</span>
-
-	<!-- </form> -->
-</div>
+<form class="form-horizontal" action="page.00.index.php?varname=bdy.02.srcrslt.php" method="post" name="form" id="form">
+	<div class="input-group">									
+		<!-- <form action="get"> -->
+		<!-- <label class="sr-only" for="src"> search </label> -->
+		
+			<input type="search" class="form-control" id="src" placeholder="Suche..." action="page.00.index.php?varname=bdy.02.srcrslt.php" name="freitext" value="<?php echo isset($_POST['freitext']) ? htmlentities($_POST['freitext']) : "" ; ?>">
+			<span class="input-group-btn">
+				<button class="btn btn-default btn-src" type="button">
+					<span class="glyphicon glyphicon-search"></span>
+				</button>
+			</span>
+		
+		<!-- </form> -->
+	</div>
+</form>
 
 <!-- extended search form -->
 <p> <a href="<?php echo trim($home["url"]).$expSuche['varname']; ?>"><?php echo $expSuche["name"]; ?></a> </p>
