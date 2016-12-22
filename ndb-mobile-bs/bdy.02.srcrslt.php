27f58ebe4f685091485d4fbac6b56d6abf3729de
Christine Lehmann <clehmann@a2er.ch>
2016-12-22 12:42:30 +0100

    nur noch Suche angezeigt

--- ndb-mobile-bs/bdy.02.srcrslt.php
+++ ndb-mobile-bs/bdy.02.srcrslt.php
@@ -1,51 +1,36 @@
-<?php
-	require_once('../../../private/verbindung.php');
-        
-	// Wenn kein 'freitext" eingegeben wurde, dann wird die Variable auf "" gesetzt
-	if(!isset($_GET['freitext'])) {
-		$_GET['freitext'] = "";
-	}
-	
-	// SQL-Abfrage für Freitextsuche, packt die Ergebnisse in $suche_freitext
-        $sql_freitext = "SELECT * FROM `tbl_noten` ";
-        $sql_freitext .= "LEFT JOIN `tbl_composer` ON `tbl_noten`.`id_composer`=`tbl_composer`.`id` ";
-        $sql_freitext .= "WHERE `title` LIKE '%".$_GET['freitext']."%' OR `name` LIKE '%".$_GET['freitext']."%' OR `firstname` LIKE '%".$_GET['freitext']."%' ";
-	$query_freitext = mysqli_query($verb, $sql_freitext) or die("Fehler:".mysqli_error($verb));
-	$suche_freitext = mysqli_fetch_assoc($query_freitext);
-	
-        // $sql_freitext = "SELECT * FROM ".$tbl_noten." "; 
-	// $sql_freitext .= "LEFT JOIN ".$tbl_komponist." ON ".$tbl_noten.".id_komponist=".$tbl_komponist.".id_komponist " ;	
-	// $sql_freitext .= "LEFT JOIN ".$tbl_verlag." ON ".$tbl_noten.".id_verlag=".$tbl_verlag.".id_verlag " ;
-	// $sql_freitext .= "LEFT JOIN ".$tbl_instrument." ON ".$tbl_noten.".id_instrument=".$tbl_instrument.".id_instrument " ;
-	// $sql_freitext .= "WHERE titel LIKE '%".$_GET['freitext']."%' OR kommentarfeld LIKE '%".$_GET['freitext']."%' OR vorname LIKE '%".$_GET['freitext']."%'
-	// 			   OR name LIKE '%".$_GET['freitext']."%' OR instrument LIKE '%".$_GET['freitext']."%' OR verlagsname LIKE '%".$_GET['freitext']."%' 
-	// 			   ORDER BY titel ASC LIMIT ".$start.",".$zeilen."; ";
-        
-        
-	// Verbindungsprobleme anzeigen
-	echo mysqli_error($verb);
-?>
+<!-- Suchergebnisse werden aufgelistet-->
 
-<h1>Suchergebnisse aufgelistet</h1>
-<h2>Klick auf Link, dann Detailansicht in entry</h2>
+<h1> Suchergebnisse </h1>
+<!-- Klick auf Link, dann Detailansicht in entry -->
 
+<?php include("incl.04.form.php"); ?>
 
+<?php if ($suche_freitext == NULL) {
+	//Wenn die Suche keine Treffer erzielt, nachfolgendes ausgeben
+	include("incl.05.noresult.php"); 
+	} else { ?>
+	<!-- Wenn die Suche Treffer erzielt, Treffer ausgeben -->
+	<div class="table">
+		<table class="table">
+		<tr>
+		<td>Titel</td>
+		<td><?php if(isset($suche_freitext)) {echo $suche_freitext ['title'];} else { echo "-"; } ?></td>
+		</tr>
+		
+		<tr>
+		<td>Komponist</td>
+		<td><?php if(isset($suche_freitext)) {echo $suche_freitext ['name'];} else { echo "-"; } ?></td>
+		<td><?php if(isset($suche_freitext)) {echo $suche_freitext ['firstname'];} else { echo "-"; } ?></td>
+		</tr>
+			
+		<tr> 
+		<td>Instrument</td>
+		<td><?php if(isset($suche_freitext)) {echo $suche_freitext ['instrument'];} else { echo "-"; } ?></td>
+		</tr>
+			
+		</table>
+	</div>
+	<? }
+	?>
 
-<div class="table">
-    <table class="table">
-	<tr>
-	<td>Titel</td>
-	<td><?php if(isset($suche_freitext)) {echo $suche_freitext ['title'];} else { echo "-"; } ?></td>
-	</tr>
-        
-        <td>Komponist</td>
-	<td><?php if(isset($suche_freitext)) {echo $suche_freitext ['name'];} else { echo "-"; } ?></td>
-        <td><?php if(isset($suche_freitext)) {echo $suche_freitext ['firstname'];} else { echo "-"; } ?></td>
-	</tr>
-        
-        <td>Instrument</td>
-	<td><?php if(isset($suche_freitext)) {echo $suche_freitext ['instrument'];} else { echo "-"; } ?></td>
-	</tr>
-        
-    </table>
-</div>
+<button onclick='history.back()'>Zur&uuml;ck zur Suche</button>
