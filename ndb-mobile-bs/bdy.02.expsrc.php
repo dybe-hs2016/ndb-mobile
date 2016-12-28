ae33609fedb2cfbb239e135d3afff4066188469a
Christine Lehmann <clehmann@a2er.ch>
2016-12-28 14:03:39 +0100

    Weitere Felder zum "schön aussehen" hinzugefügt
    Epoche, Schwierigkeitsgrad ist oki so. Instrument noch nicht ganz gut so. Die ganze Suche funktioniert aber noch nicht.

--- ndb-mobile-bs/bdy.02.expsrc.php
+++ ndb-mobile-bs/bdy.02.expsrc.php
@@ -1,4 +1,8 @@
+<!-- Expertensuche -->
+
 <h1>Expertensuche</h1>
+
+<?php include("incl.04.form.php"); ?>
 
 <h2>Suche in</h2>
 
@@ -7,24 +11,19 @@
       <section class="col-xs-12">     
 		
         <form class="form-horizontal" action="page.00.index.php?varname=bdy.02.srcrslt.php" method="post" name="form" id="form">
-			<div class="form-group">
-				<!-- Titel der Noten -->
+			<!-- Titel der Noten -->
+			<div class="form-group">¨
 				<label class="col-sm-2 control-label" for="inputTitle">Titel der Noten</label>
 				<div class="col-sm-4">
 					<input class="form-control" type="text" id="id" name="suche_title" value="<?php echo isset($_POST['suche_title']) ? htmlentities($_POST['suche_title']) : "" ; ?>">
 				</div>
 				<div class="col-sm-2">
 				<!-- Dropdown zur Auswahl ob Boolean Operator "und" oder "oder" oder "nicht" ist ; ev. auslagern ? mit incl... -->
-					<div class="dropdown">
-						<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-							Name?? <span class="caret"></span>
-						</button>
-						<ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-							<li><a href="#">und</a></li>
-							<li><a href="#">oder</a></li>
-							<li><a href="#">nicht</a></li>
-						</ul>
-					</div>
+					<select class="form-control">
+							<option>und</option>
+							<option>oder</option>
+							<option>nicht</option>
+					</select>
 				</div>
 				<div class="col-sm-4"></div>
 			</div>
@@ -38,8 +37,65 @@
 				<div class="col-sm-2">und/oder/nicht</div>
 				<div class="col-sm-4"></div>
 			</div>
+			
+			<!-- Verlag -->
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="inputTitle">Verlag</label>
+				<div class="col-sm-4">
+					<input class="form-control" type="text" id="id" name="suche_publisher" value="<?php echo isset($_POST['suche_publisher']) ? htmlentities($_POST['suche_publisher']) : "" ; ?>">
+				</div>
+				<div class="col-sm-2">und/oder/nicht</div>
+				<div class="col-sm-4"></div>
+			</div>
 		
-
+			<!-- Instrument , nicht fertig wäre gut mit Häkli-Dinger -->
+			<div class="form-group">
+				<label class="col-sm-2 control-label" for="inputTitle">Instrument</label>
+				<div class="col-sm-4">
+					<label class="checkbox-inline">
+						<?php 
+							while ($row_instrument = mysqli_fetch_assoc($result_instrument)) {
+							echo '<input type="checkbox" id="inlineCheckbox'.$row_instrument['name'].'" value="'.$row_instrument['name'].'"'.($_POST['name'] == $row_instrument['id'] ? " selected": "").'>
+							'.$row_instrument['name'].' '; } ?>
+					</label>
+				</div>
+				<div class="col-sm-2"></div>
+				<div class="col-sm-4"></div>
+			</div>
+			
+			<!-- Epoche aus Dropdown auswählen -->
+			<div class="form-group" name="epoch">
+				<label class="col-sm-2 control-label" for="inputTitle">Epoche</label>
+				<div class="col-sm-4">
+					<!-- Dropdown zur Auswahl der Epoche -->
+					<select class="form-control">
+						<?php 
+							while ($row_epoch = mysqli_fetch_assoc($result_epoch)) {
+							echo '<option value="'.$row_epoch['name'].'"'.($_POST['name'] == $row_epoch['id'] ? " selected": "").'>
+							'.$row_epoch['name'].'</option>'; } ?>
+					</select>
+				</div>
+				<div class="col-sm-2"></div>
+				<div class="col-sm-4"></div>
+			</div>
+					
+			<!-- Level aus Dropdown auswählen -->
+			<div class="form-group" name="levels">
+				<label class="col-sm-2 control-label" for="inputTitle">Schwierigkeitsgrad</label>
+				<div class="col-sm-4">
+					<!-- Dropdown zur Auswahl der Epoche -->
+					<select class="form-control">
+						<?php 
+							while ($row_levels = mysqli_fetch_assoc($result_levels)) {
+							echo '<option value="'.$row_levels['level'].'"'.($_POST['level'] == $row_levels['id'] ? " selected": "").'>
+							'.$row_levels['level'].'</option>'; } ?>
+					</select>
+				</div>
+				<div class="col-sm-2"></div>
+				<div class="col-sm-4"></div>
+			</div>
+					
+					
 		
 		<!-- pull-right - Knopf ist auf der echten Seite -->	
         <div class="form-group">
