e0652eef55ec3b1c44fd4b5324316bdbe6faa37f
Christine Lehmann <clehmann@a2er.ch>
2017-01-02 21:08:54 +0100

    id geändert

--- ndb-mobile-bs/incl.02.src.php
+++ ndb-mobile-bs/incl.02.src.php
@@ -3,18 +3,16 @@
 <!-- include SQL -->
 <?php include("incl.04.form.php"); ?>
 
-<form class="form-horizontal" action="page.00.index.php?varname=bdy.02.srcrslt.php" method="post" name="form" id="form">
+<form class="form-horizontal" action="page.00.index.php?varname=bdy.02.srcrslt.php" method="post" name="searchform" id="searchform">
 	<div class="input-group">									
 		<!-- <form action="get"> -->
 		<!-- <label class="sr-only" for="src"> search </label> -->
-		
-			<input type="search" class="form-control" id="src" placeholder="Suche..." action="page.00.index.php?varname=bdy.02.srcrslt.php" name="freitext" value="<?php echo isset($_POST['freitext']) ? htmlentities($_POST['freitext']) : "" ; ?>">
+		<input type="search" class="form-control" id="src" placeholder="Suche..." action="page.00.index.php?varname=bdy.02.srcrslt.php" name="freitext" value="<?php echo isset($_POST['freitext']) ? htmlentities($_POST['freitext']) : "" ; ?>">
 			<span class="input-group-btn">
 				<button class="btn btn-default btn-src" type="submit">
 					<span class="glyphicon glyphicon-search"></span>
 				</button>
 			</span>
-		
 		<!-- </form> -->
 	</div>
 </form>
