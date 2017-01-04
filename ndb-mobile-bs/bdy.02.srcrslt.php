cc7c94cc2d642befb563e0608b1e789c86be742d
Christine Lehmann <clehmann@a2er.ch>
2017-01-04 13:16:37 +0100

    Einbau von if/elseif bei simpler oder Expertensuche
    Expertensuche funktioniert noch nicht

--- ndb-mobile-bs/bdy.02.srcrslt.php
+++ ndb-mobile-bs/bdy.02.srcrslt.php
@@ -2,36 +2,65 @@
 
 <h1> Suchergebnisse </h1>
 <!-- Klick auf Link, dann Detailansicht in entry -->
+<?php
+	$suche_expert = NULL;
+	$suche_freitext = NULL;
+?>
 
 <?php include("incl.04.form.php"); ?>
 
-<?php if ($suche_freitext == NULL) {
-	//Wenn die Suche keine Treffer erzielt, nachfolgendes ausgeben
-	include("incl.05.noresult.php"); 
-	} else { 
-		// Wenn die Suche Treffer erzielt, Treffer ausgeben
-		do { ?>
-			<div class="table">
-				<table class="table">
-				<!-- Titel -->
-				<h2><?php if(isset($suche_freitext)) {echo $suche_freitext ['title'];} else { echo "-"; } ?></h2>
-			
-				<!-- Komponist und Instrumente -->
-				<tr>
-				<td>Komponist</td>
-				<td><?php if(isset($suche_freitext)) {echo $suche_freitext ['firstname']." ".$suche_freitext['name'];} else { echo "-"; } ?></td>
-				</tr>
-
-				<tr> 
-				<td>Instrument(e)</td>
-				<td><?php if(isset($suche_freitext)) {echo $suche_freitext ['instrument'];} else { echo "-"; } ?></td>
-				</tr>
-			<? }
-		while($suche_freitext = mysqli_fetch_assoc($query_freitext)); ?>
-				</table>
-			</div>
-		<? }
+<?php if ($suche_freitext !== NULL) {
+			// Wenn die einfache Suche Treffer erzielt, Treffer ausgeben
+				do { ?>
+					<div class="table">
+						<table class="table">
+						<!-- Titel -->
+						<h2><?php if(isset($suche_freitext)) {echo $suche_freitext['title'];} else { echo "-"; } ?></h2>
+					
+						<!-- Komponist und Instrumente -->
+						<tr>
+						<td>Komponist</td>
+						<td><?php if(isset($suche_freitext)) {echo $suche_freitext['firstname']." ".$suche_freitext['name'];} else { echo "-"; } ?></td>
+						</tr>
+						
+						<!-- Instrumente -->
+						<tr> 
+						<td>Instrument(e)</td>
+						<td><?php if(isset($suche_freitext)) {echo $suche_freitext['instrument'];} else { echo "-"; } ?></td>
+						</tr>
+					<? }
+				while($suche_freitext = mysqli_fetch_assoc($query_freitext)); ?>
+						</table>
+		</div> <?
+		} elseif ($suche_expert !== NULL) {
+			// Wenn die Experten-Suche Treffer erzielt, Treffer ausgeben
+			do { ?>
+				<div class="table">
+					<table class="table">
+					<!-- Titel -->
+					<h2><?php if(isset($suche_expert)) {echo $suche_expert['title'];} else { echo "-"; } ?></h2>
+				
+					<!-- Komponist und Instrumente -->
+					<tr>
+					<td>Komponist</td>
+					<td><?php if(isset($suche_expert)) {echo $suche_expert['firstname']." ".$suche_expert['name'];} else { echo "-"; } ?></td>
+					</tr>
+					
+					<!-- Instrumente -->
+					<tr> 
+					<td>Instrument(e)</td>
+					<td><?php if(isset($suche_expert)) {echo $suche_expert['instrument'];} else { echo "-"; } ?></td>
+					</tr>
+				<? }
+			while($suche_expert = mysqli_fetch_assoc($query_expert)); ?>
+					</table>
+				</div> <?
+		} else { 
+			//Wenn die Suche keine Treffer erzielt, nachfolgendes ausgeben
+			include("incl.05.noresult.php");
+		}
 ?>
+
 
 <button onclick='history.back()'>Zur&uuml;ck zur Suche</button>
 
