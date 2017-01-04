6c22420827a215618bc70323e8a1b4cbafedca6a
Christine Lehmann <clehmann@a2er.ch>
2017-01-04 17:38:40 +0100

    Pagination, Filtermöglichkeiten eingefügt
    Funktioniert nicht, sieht nur gut aus

--- ndb-mobile-bs/bdy.02.srcrslt.php
+++ ndb-mobile-bs/bdy.02.srcrslt.php
@@ -10,71 +10,154 @@
 <?php include("incl.04.form.php"); ?>
 
 <div class="container"> <!-- content container no 3 -->
-  <div class="row">
-      <section class="col-xs-12">
-		<div class="col-sm-6">
-			<?php if ($suche_freitext !== NULL) {
-				// Wenn die einfache Suche Treffer erzielt, Treffer ausgeben
-				do { ?>
-				<div class="table">
-					<table class="table">
-					<!-- Titel -->
-					<h2><?php if(isset($suche_freitext)) {echo $suche_freitext['title'];} else { echo "-"; } ?></h2>
-				
-					<!-- Komponist und Instrumente -->
-					<tr>
-					<td>Komponist</td>
-					<td><?php if(isset($suche_freitext)) {echo $suche_freitext['firstname']." ".$suche_freitext['name'];} else { echo "-"; } ?></td>
-					</tr>
-					
-					<!-- Instrumente -->
-					<tr> 
-					<td>Instrument(e)</td>
-					<td><?php if(isset($suche_freitext)) {echo $suche_freitext['instrument'];} else { echo "-"; } ?></td>
-					</tr>
-				<? }
-				while($suche_freitext = mysqli_fetch_assoc($query_freitext)); ?>
-					</table>
-				</div> <?
-				
-				} elseif ($suche_expert !== NULL) {
-				// Wenn die Experten-Suche Treffer erzielt, Treffer ausgeben
-				do { ?>
-					<div class="table">
+	<div class="row">
+		<section class="col-xs-12">
+			<div class="col-sm-6">
+				<?php if ($suche_freitext !== NULL) {
+					// Wenn die einfache Suche Treffer erzielt, Treffer ausgeben
+					do { ?>
+					<div class="table-responsive">
 						<table class="table">
 						<!-- Titel -->
-						<h2><?php if(isset($suche_expert)) {echo $suche_expert['title'];} else { echo "-"; } ?></h2>
+						<h2><?php if(isset($suche_freitext)) {echo $suche_freitext['title'];} else { echo "-"; } ?></h2>
 					
 						<!-- Komponist und Instrumente -->
 						<tr>
 						<td>Komponist</td>
-						<td><?php if(isset($suche_expert)) {echo $suche_expert['firstname']." ".$suche_expert['name'];} else { echo "-"; } ?></td>
+						<td><?php if(isset($suche_freitext)) {echo $suche_freitext['firstname']." ".$suche_freitext['name'];} else { echo "-"; } ?></td>
 						</tr>
 						
 						<!-- Instrumente -->
 						<tr> 
 						<td>Instrument(e)</td>
-						<td><?php if(isset($suche_expert)) {echo $suche_expert['instrument'];} else { echo "-"; } ?></td>
+						<td><?php if(isset($suche_freitext)) {echo $suche_freitext['instrument'];} else { echo "-"; } ?></td>
 						</tr>
 					<? }
-					while($suche_expert = mysqli_fetch_assoc($query_expert)); ?>
+					while($suche_freitext = mysqli_fetch_assoc($query_freitext)); ?>
 						</table>
 					</div> <?
+					
+					} elseif ($suche_expert !== NULL) {
+					// Wenn die Experten-Suche Treffer erzielt, Treffer ausgeben
+					do { ?>
+						<div class="table-responsive">
+							<table class="table">
+							<!-- Titel -->
+							<h2><?php if(isset($suche_expert)) {echo $suche_expert['title'];} else { echo "-"; } ?></h2>
+						
+							<!-- Komponist und Instrumente -->
+							<tr>
+							<td>Komponist</td>
+							<td><?php if(isset($suche_expert)) {echo $suche_expert['firstname']." ".$suche_expert['name'];} else { echo "-"; } ?></td>
+							</tr>
+							
+							<!-- Instrumente -->
+							<tr> 
+							<td>Instrument(e)</td>
+							<td><?php if(isset($suche_expert)) {echo $suche_expert['instrument'];} else { echo "-"; } ?></td>
+							</tr>
+						<? }
+						while($suche_expert = mysqli_fetch_assoc($query_expert)); ?>
+							</table>
+						</div> <?
+					
+					} else { 
+						//Wenn die Suche keine Treffer erzielt, nachfolgendes ausgeben
+						include("incl.05.noresult.php");
+					}
+				?>
+			
+				<!-- Pagination -->
+				<nav aria-label="Page navigation">
+				  <ul class="pagination">
+					<li class="disabled">
+					  <a href="#" aria-label="Search results pages">
+						<span aria-hidden="true">&laquo;</span>
+					  </a>
+					</li>
+					<li><a href="#">1</a></li>
+					<li><a href="#">2</a></li>
+					<li><a href="#">3</a></li>
+					<li>
+					  <a href="#" aria-label="Next">
+						<span aria-hidden="true">&raquo;</span>
+					  </a>
+					</li>
+				  </ul>
+				</nav>
 				
-				} else { 
-					//Wenn die Suche keine Treffer erzielt, nachfolgendes ausgeben
-					include("incl.05.noresult.php");
-				}
-			?>
-		</div>
-		<div class="col-sm-6"></div>	
+				<!-- Zurück-Button -->
+				
+				<button onclick='history.back()'>Zur&uuml;ck</button>
+				
+			</div> <!-- Ergebnisse anzeigen auf der linken Seite -->
+			
+			<!-- Einschränkungen auf der rechten Seite-->
+			<!-- Einschränkung Instrument -->
+			<div class="col-sm-3">
+				<div class="panel panel-default">
+					<div class="panel-heading">Instrument</div>
+					<div class="panel-body">
+						<?php 
+							while ($row_instrument = mysqli_fetch_assoc($result_instrument)) 
+							{
+								echo '<option value="'.$row_instrument['instrument'].'">'.$row_instrument['instrument'].'</option>';
+							} 
+						?>
+					</div>
+				</div>
+				<!-- Einschränkung Epoche -->
+				<div class="panel panel-default">
+					<div class="panel-heading">Epoche</div>
+					<div class="panel-body">
+						<?php 
+							while ($row_epoch = mysqli_fetch_assoc($result_epoch))
+							{
+								echo '<option value="'.$row_epoch['name'].'">'.$row_epoch['name'].'</option>';
+							} 
+						?>
+					</div>
+				</div>
+				<!-- Einschränkung Schwierigkeitsgrad -->
+				<div class="panel panel-default">
+					<div class="panel-heading">Schwierigkeitsgrad</div>
+					<div class="panel-body">
+						<?php 
+							while ($row_levels = mysqli_fetch_assoc($result_levels))
+							{
+								echo '<option value="'.$row_levels['level'].'">'.$row_levels['level'].'</option>';
+							}
+						?>
+					</div>
+				</div>
+				<!-- Einschränkung Stil -->
+				<div class="panel panel-default">
+					<div class="panel-heading">Stil</div>
+					<div class="panel-body">
+						<?php 
+							while ($row_musicstyle = mysqli_fetch_assoc($result_musicstyle)) 
+							{
+								echo '<option value="'.$row_musicstyle['name'].'">'.$row_musicstyle['name'].'</option>'; 
+							} 
+						?>
+					</div>
+				</div>
+				<!-- Einschränkung Anlass -->
+				<div class="panel panel-default">
+					<div class="panel-heading">Anlass</div>
+					<div class="panel-body">
+						<?php 
+							while ($row_occasion = mysqli_fetch_assoc($result_occasion)) 
+							{
+								echo '<option value="'.$row_occasion['occasion'].'">'.$row_occasion['occasion'].'</option>'; 
+							} 
+						?>
+					</div>
+				</div>	
+			</div>	<!-- Einschränkungen -->
 		</section>
 	</div> <!-- row -->
-</div> <!-- container -->
-
-<button onclick='history.back()'>Zur&uuml;ck zur Suche</button>
-
-	
+</div> <!-- container -->	
 	
 <!-- Lösung mit foreach, macht aber bei der Ausgabe von drei Feldern nicht so viel Sinn... -->	
 <?php /*
