59372990eef3a419d8ccecc30fd3f134867e6ea6
Christine Lehmann <clehmann@a2er.ch>
2017-01-14 17:32:47 +0100

    Random such in view

--- ndb-mobile-bs/incl.04.form.php
+++ ndb-mobile-bs/incl.04.form.php
@@ -22,26 +22,21 @@
 <?php
 	// Feld Titel
 	if(isset($_POST['suche_title'])) {
-		$sql_expert = "SELECT * FROM `tbl_noten` ";
-		$sql_expert .= "LEFT JOIN `tbl_composer` ON `tbl_noten`.`id_composer`=`tbl_composer`.`id` ";
-		$sql_expert .= "LEFT JOIN `noten_instrument` ON `tbl_noten`.`id`=`noten_instrument`.`id_noten` ";
-		$sql_expert .= "LEFT JOIN `tbl_instrument` ON `noten_instrument`.`id_instrument`=`tbl_instrument`.`id` ";
-		$sql_expert .= "WHERE `title` LIKE '%".$_POST['suche_title']."%' ";
+		$sql_expert = "SELECT * FROM `view_all` ";
+		$sql_expert.= "WHERE `title` LIKE '%".$_POST['suche_title']."%' ORDER BY `title` ASC ";
 		$query_expert = mysqli_query($verb, $sql_expert) or die("Fehler:".mysqli_error($verb));
 		$suche_expert = mysqli_fetch_assoc($query_expert);
 		}
 		// Feld Komponist
 		elseif(isset($_POST['suche_composer'])) {
-		$sql_expert = "SELECT * FROM `tbl_noten` ";
-		$sql_expert .= "LEFT JOIN `tbl_composer` ON `tbl_noten`.`id_composer`=`tbl_composer`.`id` ";
-		$sql_expert .= "LEFT JOIN `noten_instrument` ON `tbl_noten`.`id`=`noten_instrument`.`id_noten` ";
-		$sql_expert .= "LEFT JOIN `tbl_instrument` ON `noten_instrument`.`id_instrument`=`tbl_instrument`.`id` ";
-		$sql_expert .= "WHERE `name` LIKE '%".$_POST['suche_composer']."%' OR `firstname` LIKE '%".$_POST['suche_composer']."%' ";	
+		$sql_expert = "SELECT * FROM `view_all` ";
+		$sql_expert.= "WHERE `composerFullname` LIKE '%".$_POST['suche_composer']."%' ORDER BY `title` ASC ";
 		$query_expert = mysqli_query($verb, $sql_expert) or die("Fehler:".mysqli_error($verb));
 		$suche_expert = mysqli_fetch_assoc($query_expert);
 		}
 		elseif(isset($_POST['suche_publisher'])) {
-		$sql_expert = "SELECT `name` FROM ".$tbl_composer." WHERE `name` LIKE '%".$_POST["suche_composer"]."%' ";	
+		$sql_expert = "SELECT * FROM `view_all` ";
+		$sql_expert.= "WHERE `publisherName` LIKE '%".$_POST['suche_publisher']."%' ORDER BY `title` ASC ";
 		$query_expert = mysqli_query($verb, $sql_expert) or die("Fehler:".mysqli_error($verb));
 		$suche_expert = mysqli_fetch_assoc($query_expert);
 		}
@@ -53,38 +48,38 @@
 
 <?php
 	// Instrumenten-Auswahl mit Checkboxen
-	$sql_instrument = "SELECT id, name FROM ".$tbl_instrument." ORDER BY id";	
-	$query_instrument = mysqli_query($verb, $sql_instrument) or die("Fehler:".mysqli_error($verb));
+	$sql_instrument = "SELECT id, name FROM ".$tbl_instrument." ORDER BY name ASC";	
+	$result_instrument = mysqli_query($verb, $sql_instrument) or die("Fehler:".mysqli_error($verb));
 ?>
 
 <?php
 	// Epoche-Dropdown
-	$sql_epoch = "SELECT id, name FROM ".$tbl_epoch." ORDER BY id DESC";	
-	$query_epoch = mysqli_query($verb, $sql_epoch) or die("Fehler:".mysqli_error($verb));
+	$sql_epoch = "SELECT id, name FROM ".$tbl_epoch." ORDER BY id ASC";	
+	$result_epoch = mysqli_query($verb, $sql_epoch) or die("Fehler:".mysqli_error($verb));
 ?>
 
 <?php
 	// Level-Dropdown
-	$sql_levels = "SELECT id, level FROM ".$tbl_levels." ORDER BY id DESC";	
-	$query_levels = mysqli_query($verb, $sql_levels) or die("Fehler:".mysqli_error($verb));
+	$sql_levels = "SELECT id, level FROM ".$tbl_levels." ORDER BY id ASC";	
+	$result_levels = mysqli_query($verb, $sql_levels) or die("Fehler:".mysqli_error($verb));
 ?>
 
 <?php
 	// Musicstyle-Dropdown
-	$sql_musicstyle = "SELECT id, name FROM ".$tbl_musicstyle." ORDER BY id DESC";	
-	$query_musicstyle = mysqli_query($verb, $sql_musicstyle) or die("Fehler:".mysqli_error($verb));
+	$sql_musicstyle = "SELECT id, name FROM ".$tbl_musicstyle." ORDER BY id ASC";	
+	$result_musicstyle = mysqli_query($verb, $sql_musicstyle) or die("Fehler:".mysqli_error($verb));
 ?>
 
 <?php
 	// Occaion-Dropdown
-	$sql_occasion = "SELECT id, occasion FROM ".$tbl_occasion." ORDER BY id DESC";	
-	$query_occasion = mysqli_query($verb, $sql_occasion) or die("Fehler:".mysqli_error($verb));
+	$sql_occasion = "SELECT id, occasion FROM ".$tbl_occasion." ORDER BY id ASC";	
+	$result_occasion = mysqli_query($verb, $sql_occasion) or die("Fehler:".mysqli_error($verb));
 ?>
 
 
 <!-- Random-Befehl für Zufallstreffer -->
 <?php
-	$sql_random = "SELECT * FROM `tbl_noten` ORDER BY RAND() LIMIT 1";
+	$sql_random = "SELECT * FROM `view_all` ORDER BY RAND() LIMIT 1";
 	$query_random = mysqli_query($verb, $sql_random) or die("Fehler:".mysqli_error($verb));
 	$random = mysqli_fetch_assoc($query_random);
 	echo mysqli_error($verb);
