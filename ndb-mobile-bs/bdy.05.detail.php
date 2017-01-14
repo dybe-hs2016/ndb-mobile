8b136b8d7d113d0e2e6bbccac8fd98b1283173c5
Christine Lehmann <clehmann@a2er.ch>
2017-01-14 14:36:56 +0100

    sql im dok und button dazu
     outsourcen schaff ich nicht, er findet dann die variablen nicht

--- ndb-mobile-bs/bdy.05.detail.php
+++ ndb-mobile-bs/bdy.05.detail.php
@@ -1,28 +1,51 @@
 <!-- Detailseite -->
 
-
-<?php include("incl.04.form.php"); ?>
+<?php 
+	$sql_detail = "SELECT * FROM `view_all` WHERE `id`='".$_GET['id']."' ";	
+	$query_detail = mysqli_query($verb, $sql_detail) or die("Fehler:".mysqli_error($verb));
+	$result_detail = mysqli_fetch_assoc($query_detail);
+	echo mysqli_error($verb);
+	?>
 
 <?php 
-	echo '<h1>'.$freitext['title'].'</h1>';
+	echo '<h1>'.$result_detail['title'].'</h1>';
 ?>
 
-	<div class="table">
-		<table class="table">
-			<!-- <tr> ID BEZEICHNUNG METADATENFELD
-				<th class="key">BEZEICHUNG</th>
-				<th class="val">INHALT</th>
-			</tr> -->
-
-			<?php 
-				foreach ($freitext as $key => $val) {			
-					echo '<tr> 
-						<th class="key">'.$key.'</th>
-						<th class="val">'.$val.'</th> 
-					</tr>';
-				}
-			?>
-		</table>
-	</div>	
-
-	
+<div class="container"> <!-- content container no 3 -->
+	<div class="row">
+		<div class="col-xs-12 col-sm-6">
+			<div class="table">
+				<table class="table">
+					<!-- <tr> ID BEZEICHNUNG METADATENFELD
+						<th class="key">BEZEICHUNG</th>
+						<th class="val">INHALT</th>
+					</tr> -->
+					<?php 
+						foreach ($result_detail as $key => $val) {
+							if ($val !== NULL) {
+								echo '<tr> 
+									<th class="key">'.$key.'</th>
+									<th class="val">'.$val.'</th>
+									</tr>';
+							} else {
+								'<tr>
+								<th class="key">'.$key.'</th>
+								<th class="val">-</th>
+								</tr>';
+							}
+						}
+					?>
+				</table>
+			</div>
+			
+			<!-- Button um zu Bearbeiten -->
+			<form class="form-horizontal" action="page.00.index.php?varname=bdy.04.nErfassen.php" method="post" name="form" id="form">
+				<div class="input-group">									
+					<button class="btn btn-primary pull-left btn-sm btn-block">Bearbeiten</button>
+					<input name="id" type="hidden" value="<?php echo $_GET["id"]; ?>">					
+				</div>
+			</form>
+			
+		</div> <!-- div col -->
+	</div> <!-- div row -->
+</div> <!-- div container -->
