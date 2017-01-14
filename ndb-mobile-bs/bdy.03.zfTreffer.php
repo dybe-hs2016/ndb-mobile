cdf08cc75ea9f93a543bc95f62e2a5c932946558
Christine Lehmann <clehmann@a2er.ch>
2017-01-14 14:36:17 +0100

    div container eingefügt, sm/xs eingefügt, buttons dazu

--- ndb-mobile-bs/bdy.03.zfTreffer.php
+++ ndb-mobile-bs/bdy.03.zfTreffer.php
@@ -7,21 +7,41 @@
 	echo '<h1>'.$random['title'].'</h1>';
 ?>
 
-	<div class="table">
-		<table class="table">
-			<!-- <tr> ID BEZEICHNUNG METADATENFELD
-				<th class="key">BEZEICHUNG</th>
-				<th class="val">INHALT</th>
-			</tr> -->
+<div class="container"> <!-- content container no 3 -->
+	<div class="row">
+		<div class="col-xs-12 col-sm-6">
+			<div class="table">
+				<table class="table">
+					<!-- <tr> ID BEZEICHNUNG METADATENFELD
+						<th class="key">BEZEICHUNG</th>
+						<th class="val">INHALT</th>
+					</tr> -->
 
-			<?php 
-				foreach ($random as $key => $val) {			
-					echo '<tr> 
-						<th class="key">'.$key.'</th>
-						<th class="val">'.$val.'</th> 
-					</tr>';
-				}
-			?>
-		</table>
-	</div>	
+					<?php 
+						foreach ($random as $key => $val) {
+							if ($val !== NULL) {
+								echo '<tr> 
+									<th class="key">'.$key.'</th>
+									<th class="val">'.$val.'</th>
+									</tr>';
+							} else {
+								'<tr>
+								<th class="key">'.$key.'</th>
+								<th class="val">-</th>
+								</tr>';
+							}
+						}
+					?>
+				</table>
+			</div>	
 
+			<!-- Button für erneuten Treffer -->
+			<form class="form-horizontal" action="javascript:location.reload()" method="post" name="random" id="random">
+				<div class="input-group">									
+					<button class="btn btn-primary pull-left btn-sm btn-block">Neuer Zufallstreffer</button>
+				</div>
+			</form>
+		
+		</div> <!-- div col -->
+	</div> <!-- div row -->
+</div> <!-- div container -->
