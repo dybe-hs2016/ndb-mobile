b63da7657b058d7ecf4ace92974d644f4c2a9bf9
Christine Lehmann <clehmann@a2er.ch>
2017-01-14 17:32:27 +0100

    if empty dann "-"

--- ndb-mobile-bs/bdy.05.detail.php
+++ ndb-mobile-bs/bdy.05.detail.php
@@ -7,6 +7,7 @@
 	echo mysqli_error($verb);
 	?>
 
+
 <?php 
 	echo '<h1>'.$result_detail['title'].'</h1>';
 ?>
@@ -16,24 +17,80 @@
 		<div class="col-xs-12 col-sm-6">
 			<div class="table">
 				<table class="table">
-					<!-- <tr> ID BEZEICHNUNG METADATENFELD
-						<th class="key">BEZEICHUNG</th>
-						<th class="val">INHALT</th>
-					</tr> -->
 					<?php 
-						foreach ($result_detail as $key => $val) {
-							if ($val !== NULL) {
-								echo '<tr> 
-									<th class="key">'.$key.'</th>
-									<th class="val">'.$val.'</th>
-									</tr>';
+						do { ?>
+
+						<!-- Komponist -->						
+						<tr>
+						<td>Komponist</td>
+						<td><?php if(!empty($result_detail['composerFullname'])) {echo $result_detail['composerFullname'];} else { echo "-"; } ?></td>
+						</tr>
+						<!-- Verlag -->
+						<tr> 
+						<td>Verlag</td>
+						<td><?php if(!empty($result_detail['publisherName'])) {echo $suche_expert['publisherName'];} else { echo "-"; } ?></td>
+						</tr>
+						<!-- Signatur -->
+						<tr> 
+						<td>Signatur</td>
+						<td><?php if(!empty($result_detail['signature'])) {echo $suche_expert['signature'];} else { echo "-"; } ?></td>
+						</tr>
+						<!-- Epoche -->
+						<tr> 
+						<td>Epoche</td>
+						<td><?php if(!empty($result_detail['epochName'])) {echo $suche_expert['epochName'];} else { echo "-"; } ?></td>
+						</tr>
+						<!-- Instrumente -->
+						<tr> 
+						<td>Instrument(e)</td>
+						<td><?php if(!empty($result_detail['instruments'])) {echo $suche_expert['instruments'];} else { echo "-"; } ?></td>
+						</tr>
+						<!-- Stil -->
+						<tr> 
+						<td>Stil</td>
+						<td><?php if(!empty($result_detail['musicstyleName'])) {echo $suche_expert['musicstyleName'];} else { echo "-"; } ?></td>
+						</tr>
+						<!-- Schwierigkeitsgrad -->
+						<tr> 
+						<td>Schwierigkeitsgrad</td>
+						<td><?php if(!empty($result_detail['level'])) {echo $suche_expert['level'];} else { echo "-"; } ?></td>
+						</tr>
+						<!-- Anlass -->
+						<tr> 
+						<td>Anlass</td>
+						<td><?php if(!empty($result_detail['occasion'])) {echo $suche_expert['occasion'];} else { echo "-"; } ?></td>
+						</tr>
+						<!-- Youtube-Video -->
+						<tr> 
+						<td>Youtube-Video</td>
+						<td><?php
+							if ($random['linktomusic'] == NULL) {
+								echo "-";
 							} else {
-								'<tr>
-								<th class="key">'.$key.'</th>
-								<th class="val">-</th>
-								</tr>';
+								echo "<a href='".$random['linktomusic']."' target='_blank'>Link</a>";
 							}
+						?></td>
+						</tr>
+						<!-- Link zu Sheetmusic -->
+						<tr> 
+						<td>Link zu Sheetmusic</td>
+						<td><?php
+							if ($random['linktosheet'] == NULL) {
+								echo "-";
+							} else {
+								echo "<a href='".$random['linktosheet']."' target='_blank'>Link</a>";
+							}
+						?></td>
+						</tr>
+						<!-- Kommentar -->
+						<tr> 
+						<td>Kommentar</td>
+						<td><?php if(!empty($result_detail['comment'])) {echo $suche_expert['comment'];} else { echo "-"; } ?></td>
+						</tr>
+
+						<?php	
 						}
+						while($result_detail = mysqli_fetch_assoc($query_detail));	
 					?>
 				</table>
 			</div>
