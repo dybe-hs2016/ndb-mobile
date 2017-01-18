286d6ce846a28f6da4c2fdf3bd1dab15cd87655d
Christine Lehmann <clehmann@a2er.ch>
2017-01-18 23:49:18 +0100

    new file, not finished
    mit dene Span-Element-Glyphicons gohts eifach nöd...

---
+++ ndb-mobile-bs/bdy.05.benEinst.php
@@ -0,0 +1,43 @@
+<h1>Benutzereinstellungen</h1>
+
+<div class="col-xs-12 col-sm-8">
+	<!-- Benutzereinstellungen ändern -->
+	<form>
+		<!-- Benutzername -->
+		<div class="row">
+		  <div class="col-sm-8">
+			<div class="input-group">
+				<div class="input-group-btn">
+					<button type="button" class="btn btn-default">
+						<span class="glyphicons glyphicons-pencil" aria-hidden="true"></span>Tuet?
+					</button>
+					<input type="" class="form-control" id="username" placeholder="Benutzername...">
+				</div>
+			</div><!-- /input-group -->
+		  </div><!-- /.col-sm-8 -->
+		</div><!-- /.row -->
+
+		
+		<!-- Email -->
+		<div class="input-group">
+			<span class="input-group-addon" id="basic-addon2">@</span>
+			<input type="email" class="form-control" placeholder="E-Mail" aria-describedby="basic-addon2">
+		</div>
+		
+		<!-- PASSWORD -->
+		<div class="input-group">
+			<input type="password" class="form-control" id="pwd" placeholder="Passwort...">
+			<span class="glyphicons glyphicons-keys" aria-hidden="true">Schlüssel</span>
+			<span class="glyphicons glyphicons-user" aria-hidden="true">Benutzer</span>
+		</div>
+		
+		<!-- Ändern -->
+		<button type="submit" class="btn btn-default btn-li">&Auml;ndern</button>
+	</form>
+</div>		
+
+
+
+
+    
+
