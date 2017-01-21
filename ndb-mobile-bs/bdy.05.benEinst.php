f6c8791b940b200247a535da2309ce9c1c657716
Christine Lehmann <clehmann@a2er.ch>
2017-01-21 15:10:57 +0100

    key-glyphicon fehlt noch

--- ndb-mobile-bs/bdy.05.benEinst.php
+++ ndb-mobile-bs/bdy.05.benEinst.php
@@ -1,31 +1,35 @@
 <h1>Benutzereinstellungen</h1>
 
-<div class="col-xs-12 col-sm-8">
+<div class="col-xs-12 col-sm-8 col-md-8" "col-lg-8">
+	<p>Hier können Sie ihre Benutzerdaten verwalten und ändern.</p>
 	<!-- Benutzereinstellungen ändern -->
 	<form>
 		<!-- Benutzername -->
-		<div class="row">
-		  <div class="col-sm-8">
-			<div class="input-group">
-				<div class="input-group-btn">
-					<button type="button" class="btn btn-default">
-						<span class="glyphicons glyphicons-pencil" aria-hidden="true"></span>Tuet?
-					</button>
-					<input type="" class="form-control" id="username" placeholder="Benutzername...">
-				</div>
-			</div><!-- /input-group -->
-		  </div><!-- /.col-sm-8 -->
-		</div><!-- /.row -->
+		<div class="input-group">
+			<span class="input-group-addon glyphicon glyphicon-user"></span>
+			<input type="" class="form-control" id="username" placeholder="Benutzername...">
+			<span class="input-group-btn">
+				<button class="btn btn-default btn-src" type="submit">
+					<span class="glyphicon glyphicon-pencil"></span>
+				</button>
+			</span>
+		</div>
 
-		
+
 		<!-- Email -->
 		<div class="input-group">
 			<span class="input-group-addon" id="basic-addon2">@</span>
 			<input type="email" class="form-control" placeholder="E-Mail" aria-describedby="basic-addon2">
+			<span class="input-group-btn">
+				<button class="btn btn-default btn-src" type="submit">
+					<span class="glyphicon glyphicon-pencil"></span>
+				</button>
+			</span>
 		</div>
 		
 		<!-- PASSWORD -->
 		<div class="input-group">
+			<span class="input-group-addon glyphicons glyphicon-key"></span>
 			<input type="password" class="form-control" id="pwd" placeholder="Passwort...">
 			<span class="input-group-btn">
 				<button class="btn btn-default btn-src" type="submit">
@@ -33,13 +37,6 @@
 				</button>
 			</span>
 		</div>
-		<div class="input-group">
-			<span class="glyphicons glyphicons-keys" aria-hidden="true">Schlüssel</span>
-			<span class="glyphicons glyphicons-user" aria-hidden="true">Benutzer</span>
-		</div>
-		
-		<!-- Ändern -->
-		<button type="submit" class="btn btn-default btn-li">&Auml;ndern</button>
 	</form>
 </div>		
 
