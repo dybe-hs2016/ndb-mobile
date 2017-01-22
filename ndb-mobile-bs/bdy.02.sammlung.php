3683b60fc6f8ff5c79dca7d05306829cca80e7d1
Christine Lehmann <clehmann@a2er.ch>
2017-01-22 21:12:51 +0100

    seite erstellt

---
+++ ndb-mobile-bs/bdy.02.sammlung.php
@@ -0,0 +1,6 @@
+<!-- Meine Sammlung -->	
+<h1>Meine Sammlung</h1>
+
+<p>Diese Seite befindet sich im Aufbau. <br>
+Hier werden Sie später verschiedene Suchanfragen oder Treffer einer Suchanfrage speichern und damit schneller abrufen können.</p>
+
